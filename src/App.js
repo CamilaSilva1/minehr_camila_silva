b883917ffca1739fba161a2b9c81b977b21c2043
CamilaSilva <camiladasilvaassis1@gmail.com>
2022-06-13 12:16:16 -0300

    feature: Graficos criados e adicionados por meio de componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+// Created By Camila Silva
+// Since 13/06/2022
+// Using ApexCharts.js with React.js
+// This script is responsable for render the components of the chart created 
+// Main script
 
-function App() {
-  return (
-    <div>
-      <h1>Charts</h1>
-    </div>
-  );
+// imports
+import React, { Component } from 'react';
+
+// importing modules and components
+import BarChart from './components/BarChart.jsx';
+import ScatterChart from './components/ScatterChart.jsx'
+
+// renderizing the components on the main page
+class App extends React.Component{
+      render(){
+          return (
+              <div>
+                {/* bar chart component */}
+                <BarChart />
+                <ScatterChart />
+              </div>
+          );
+      }
 }
 
 export default App;
